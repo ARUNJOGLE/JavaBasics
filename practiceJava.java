4cea34b44176f8c056df3b3fde5a04c5cd2349e4
ARUNJOGLE <85990257+ARUNJOGLE@users.noreply.github.com>
2024-08-19 22:45:47 +0530

    practiceJava.java
    Task 1 : Adams Number
    Updated on : 19/08/2024
    Due Date : 21/08/2024

--- practiceJava.java
+++ practiceJava.java
@@ -1,19 +1,21 @@
+/*
+Task 1: Adams Number : Adam number is a number when reversed, the square of the number and the square of the reversed number should be numbers which are reverse of each other.
+        Example : input: 12 --> (12)^2 = 144, reverse of 12 --> 21 , (21)^2=441, reverse of ((21)^2) i.e, reverse(441)
+                                if 12^2 == reverse(reverse(n^2)) then it is an adams number else not!
+Due Date : 21/08/2024 , Wednesday
+*/
+
+
+
 package com.arun;
 import java.util.*;
 public class practiceJava {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-
-        String name = "Fantastic";
-
-        changeName(name);
-        System.out.println(name);
+       
 
 
 
     }
 
-    static void changeName(String name) {
-        name="hi";
-    }
+
 }
