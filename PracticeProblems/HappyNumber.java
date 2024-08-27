689baa781cf71e941d3676ee7f7c7246fefb558c
ARUNJOGLE <85990257+ARUNJOGLE@users.noreply.github.com>
2024-08-27 22:57:20 +0530

    HappyNumber.java
    Bug fixed -  Happy Number logic

--- PracticeProblems/HappyNumber.java
+++ PracticeProblems/HappyNumber.java
@@ -7,32 +7,32 @@ public class HappyNumber {
         Scanner in = new Scanner(System.in);
         while (true) {
             System.out.print("Enter a number to check : ");
-            int n = in.nextInt(); // let say input n: 19
+            int n = in.nextInt(); // n:19
 
-            int squareDigit = squareDigits(n); // 82 // 64 // 1
+            int temp = n; // temp : 19
+            temp = squareDigit(temp); // temp : 19 --> squareDigit(19) --> return sum
 
-            while (squareDigit != 1 && n != 0) { // 82 != 1 // 64 != 1 // 1 != 1 false
-                squareDigit = squareDigits(squareDigit); // 82 // 64 // skip
-
+            while (temp != 1 && temp != 4) { // return 1 or 4 true
+                temp = squareDigit(temp);
             }
-
-            if (squareDigit == 1) { // 1 == 1 // true
+            if (temp == 1) {
                 System.out.println(n + " is a Happy Number");
             } else {
                 System.out.println(n + " is not a Happy Number");
             }
-
         }
+
     }
 
-    static int squareDigits(int n) { // n:19 // 82 // 64
-        int rem=0;
-        int iSquare=0;
-        while(n>0){ // 19 > 0 condition true
-                rem = n % 10; // getting individual digits
-                iSquare += (rem * rem); // 81 + 1 = 82 //works until isSquare reaching 1
-                n /= 10; // getting square of next digit
-            }
-        return iSquare; // 82 // 64 // 1 // exiting function
+//    squareDigit Method
+    static int squareDigit(int temp) {
+        int rem = 0;
+        int sum = 0;
+        while(temp > 0){
+            rem = temp % 10;
+            sum+=(rem*rem);
+            temp/=10;
+        }
+        return sum; // 1^2 + 9^2 = 1+81 = 82 (not equal to 1)
     }
 }
